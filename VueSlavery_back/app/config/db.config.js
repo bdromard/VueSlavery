0141b33ee66a6f5e11fa1c57895713243751c62f
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-06-04 19:02:12 +0200

    Changing repo structure

---
+++ VueSlavery_back/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_LOGIN,
+    PASSWORD : process.env.DB_PASSWORD,
+    DB: "VueSlavery",
+    dialect: "mariadb",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
