7fe4ed60e4632e0060c124f52817bfbc57b9df56
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-06-23 16:29:00 +0200

    Added routes in router, moved index.html to public

--- VueSlavery_front/src/router.js
+++ VueSlavery_front/src/router.js
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 
 const routes = [
     {
-        path: "/",
+        path: "/slaves",
         alias: "/slaves",
         name: "slaves",
         // Route to fill
@@ -15,9 +15,44 @@ const routes = [
         component: () => import("./components/")
     },
     {
-        path: "/add",
-        name: "add",
+        path: "/slaves/add",
+        name: "add-slaves",
         component: () => import("./components/AddSlave")
+    },
+    {
+        path: "/owners",
+        alias: "/owners",
+        name: "owners",
+        component: () => import("./components/")
+    },
+    {
+        path: "/owners/:id",
+        name: "owners-details",
+        // Route to fill
+        component: () => import("./components/")
+    },
+    {
+        path: "/owners/add",
+        name: "add-owners",
+        component: () => import("./components/AddSlave")
+    },
+    {
+        path: "/texts",
+        alias: "/texts",
+        name: "texts",
+        component: () => import("./components/")   
+    },
+    {   
+        path: "/cities",
+        alias: "/cities",
+        name: "cities",
+        component: () => import("./components/")  
+    },
+    {
+        path: "/archives",
+        alias: "/archives",
+        name: "archives",
+        component: () => import("./components/") 
     }
 ];
 
