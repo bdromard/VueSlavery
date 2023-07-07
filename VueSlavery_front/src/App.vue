ad69454248e59db63d6fc308068f8cfc139166b0
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-07-07 15:31:23 +0200

    Reinstalled Vue

--- VueSlavery_front/src/App.vue
+++ VueSlavery_front/src/App.vue
@@ -4,27 +4,44 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <div id="app">
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">bdromard</router-link>
-      <div class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link to="/slaves" class="nav-link">Slaves</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link to="/add" class="nav-link">Add</router-link>
-        </li>
-      </div>
-    </nav>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="container mt-3">
-      <router-view />
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
     </div>
-  </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<script>
-export default {
-  name: "app"
-};
-</script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
