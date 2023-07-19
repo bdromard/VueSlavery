247d091cd1b5e4ff33f940d5df1228a1eedc74d3
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-07-19 17:47:18 +0200

    Navbar component

---
+++ VueSlavery_front/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="w-auto h-28 bg-indigo-500 flex flex-row">
+      <img alt="VueSlavery logo" />
+
+      <button class="w-40 h-14 bg-orange-500 rounded">Database</button>
+      <button class="w-40 h-14 bg-orange-500 rounded">Chapters</button>
+    </div>    
+  </nav>
+</template> 
