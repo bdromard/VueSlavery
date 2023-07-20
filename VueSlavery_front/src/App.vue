04e34bb35867a78eef58a9998e314bcd52762acb
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-07-20 17:46:37 +0200

    WIP AdminForm component

--- VueSlavery_front/src/App.vue
+++ VueSlavery_front/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
-
+import AdminForm from './components/AdminForm.vue'
 </script>
 
 <template>
 
   <main>
     <Navbar />
+    <AdminForm />
   </main>
 </template>
 
---
+++ VueSlavery_front/src/components/AdminForm.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <form id="admin-form">
+    <div class="admin-form bg-red-500 w-1/2 h-1/2">
+      <label for="table">Select table</label>
+      <input id="table">
+    </div>
+  </form>
+</template>
