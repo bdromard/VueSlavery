694787f68865c679efca8f530e4c154993a18ddf
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-07-21 16:31:26 +0200

    Route and component logic to get columns from clicked table

--- VueSlavery_back/app/routes/database.routes.js
+++ VueSlavery_back/app/routes/database.routes.js
@@ -4,19 +4,32 @@ module.exports = app => {
   let router = require("express").Router();
 
   
-  const getTables = async (req, res) => {
+  const getTablesNames = async (req, res) => {
   await db.sequelize.query("SHOW TABLES FROM `VueSlaveryDB`;", {
     type: db.sequelize.QueryTypes.SELECT
   })
     .then((result) => {
-    res.json(result)
+    res.json(result);
       })
     .catch((error) => {
-      console.log('Query ERROR', error)
+      res.status(500).json('Query ERROR', error);
     })
-  }
+  };
 
-  router.get('/alltables', getTables)
+  const getColumnsNames = async (req, res) => {
+    try {
+    const attributes = await db.sequelize.models[`${req.body.model}`].getAttributes()
+    res.json(attributes)
+    } catch(error) {
+      res.status(500).json('Cannot get attributes from table', error)
+    }
+  };
+
+  // GET route to list all tables' names from database
+  router.get('/alltables', getTablesNames)
+
+  // GET route to list columns from a table
+  router.post('/allcolumns', getColumnsNames)
 
   app.use('/api/database', router)
   }
--- VueSlavery_front/src/components/AdminForm.vue
+++ VueSlavery_front/src/components/AdminForm.vue
@@ -2,17 +2,48 @@
 import { ref } from 'vue';
 import { onMounted } from 'vue';
 
-let databaseApi = 'http://localhost:5174/api/database/allTables'
+let databaseApi = 'http://localhost:5174/api/database/'
 const tables = ref()
+const columns = ref()
 
 const getTables = async () => {
-  const response = await fetch(databaseApi, {
+
+  try { 
+  const response = await fetch(`${databaseApi}allTables`, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'},
-  })
+  });
+
   const allTables = await response.json()
   tables.value = allTables
-}
+  }
+
+  catch(error) {
+    console.log(error)
+  };
+};
+
+const getColumns = async (model) => {
+
+  try {
+
+  const response = await fetch(`${databaseApi}allColumns`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+      model: model
+    
+  })
+  });
+
+  const allColumns = await response.json()
+  columns.value = allColumns
+  } 
+
+  catch(error) {
+    console.log(error)
+  };
+};
 
 onMounted(() => {
   getTables()
@@ -24,10 +55,9 @@ onMounted(() => {
   <form id="admin-form" class="bg-red-500 w-1/2 h-60">
     <div class="admin-form">
       <label for="tables">Select table</label>
-<!--       <button @click.prevent="getTables" class="bg-green-700"></button> -->
       <select name="tables">
         <option value="Tables" selected>Tables</option>
-        <option v-for="value in tables">{{ value['Tables_in_VueSlaveryDB'] }}</option>
+        <option v-for="value in tables" @click.prevent="getColumns(value['Tables_in_VueSlaveryDB'])">{{ value['Tables_in_VueSlaveryDB'] }}</option>
       </select> 
     </div>
   </form>
