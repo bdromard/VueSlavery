3fef0f44d72ff7a8a12b0a4b47bd84b3df319047
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-07-21 11:52:18 +0200

    Working dynamic select all tables from database

--- VueSlavery_front/src/components/AdminForm.vue
+++ VueSlavery_front/src/components/AdminForm.vue
@@ -1,26 +1,34 @@
 <script setup>
 import { ref } from 'vue';
+import { onMounted } from 'vue';
 
 let databaseApi = 'http://localhost:5174/api/database/allTables'
-const tables = ref([])
+const tables = ref()
 
 const getTables = async () => {
   const response = await fetch(databaseApi, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'},
-  }).then((data) => {
-      let tab = data.json()
-    })
+  })
+  const allTables = await response.json()
+  tables.value = allTables
 }
 
-console.log(getTables())
+onMounted(() => {
+  getTables()
+})
+
 </script>
 
 <template>
-  <form id="admin-form">
-    <div class="admin-form bg-red-500 w-1/2 h-1/2">
-      <label for="table">Select table</label>
-      <input id="table">
+  <form id="admin-form" class="bg-red-500 w-1/2 h-60">
+    <div class="admin-form">
+      <label for="tables">Select table</label>
+<!--       <button @click.prevent="getTables" class="bg-green-700"></button> -->
+      <select name="tables">
+        <option value="Tables" selected>Tables</option>
+        <option v-for="value in tables">{{ value['Tables_in_VueSlaveryDB'] }}</option>
+      </select> 
     </div>
   </form>
 </template>
